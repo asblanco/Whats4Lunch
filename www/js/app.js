eb324a180ae60583a250e3d6887de146a5f7605f
Andrea Sánchez <andreasanchezblanco@gmail.com>
2016-05-07 23:00:22 +0200

    Remove images
    Remove unnecessary images, comments and css class.

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,6 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'app' is the name of this angular module (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'app.controllers' is found in controllers folder
 angular
 .module('w4l', ['ionic', 'backand', 'w4l.controllers', 'underscore'])
 
-/*$ImageCacheFactory on .run to preload on cache memory the firsts images that the app displays avoiding load the page first without any image*/
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard for form inputs)
@@ -84,7 +77,7 @@ angular
     //if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/home');
     
-    //Activate native scroll on all the app
+    //Activate android native scroll on all the app
     if(!ionic.Platform.isIOS())
         $ionicConfigProvider.scrolling.jsScrolling(false); 
 });
